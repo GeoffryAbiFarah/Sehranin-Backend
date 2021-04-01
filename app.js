4dd74d9e302bb020e790a1f087d8a692c53393e8
Marven-AC <marven901@gmail.com>
2021-04-01 13:26:30 +0300

    Solved deprecation

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 const url = process.env.MONGO_URL;
-const connect = mongoose.connect(url);
+const connect = mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true});
 
 connect
   .then((db) => {
